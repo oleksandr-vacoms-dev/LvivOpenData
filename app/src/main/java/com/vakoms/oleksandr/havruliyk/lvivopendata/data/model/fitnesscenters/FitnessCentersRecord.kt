36c18158ecfd559db478230698295b77d5ea11d4
oleksandr-vakoms-dev <oleksandr.havruliyk@vakoms.com>
2019-07-31 18:18:10 +0300

    add FitnessCenters models

---
+++ app/src/main/java/com/vakoms/oleksandr/havruliyk/lvivopendata/data/model/fitnesscenters/FitnessCentersRecord.kt
@@ -0,0 +1,23 @@
+package com.vakoms.oleksandr.havruliyk.lvivopendata.data.model.fitnesscenters
+
+import com.google.gson.annotations.SerializedName
+
+data class FitnessCentersRecord(
+    @SerializedName("_id") var id: Int,
+    @SerializedName("district") var district: String,
+    @SerializedName("street") var street: String,
+    @SerializedName("building") var building: String,
+    @SerializedName("name") var name: String,
+    @SerializedName("enterpreneur_name") var enterpreneurName: String,
+    @SerializedName("square") var square: String,
+    @SerializedName("code_phone_number") var codePhoneNumber: String,
+    @SerializedName("phone_number_1") var phoneNumber1: String,
+    @SerializedName("phone_number_2") var phoneNumber2: String,
+    @SerializedName("cellphone_number_1") var cellphoneNumber1: String,
+    @SerializedName("cellphone_number_2") var cellphoneNumber2: String,
+    @SerializedName("cellphone_number_3") var cellphoneNumber3: String,
+    @SerializedName("hours_of_work_(weekdays)") var hoursOfWorkWeekdays: String,
+    @SerializedName("hours_of_work_(weekdays)_brake") var hoursOfWorkWeekdaysBrake: String,
+    @SerializedName("hours_of_work_(saturday)") var hoursOfWorkSaturday: String,
+    @SerializedName("hours_of_work_(sunday)") var hoursOfWorkSunday: String
+)
---
+++ app/src/main/java/com/vakoms/oleksandr/havruliyk/lvivopendata/data/model/fitnesscenters/FittnesCentersResult.kt
@@ -0,0 +1,16 @@
+package com.vakoms.oleksandr.havruliyk.lvivopendata.data.model.fitnesscenters
+
+import com.google.gson.annotations.SerializedName
+import com.vakoms.oleksandr.havruliyk.lvivopendata.data.model.Field
+import com.vakoms.oleksandr.havruliyk.lvivopendata.data.model.Links
+import com.vakoms.oleksandr.havruliyk.lvivopendata.data.model.market.MarketsRecord
+
+data class FittnesCentersResult(
+    @SerializedName("include_total") var includeTotal: Boolean,
+    @SerializedName("resource_id") var resourceId: String,
+    @SerializedName("fields") var fields: List<Field>,
+    @SerializedName("records_format") var recordsFormat: String,
+    @SerializedName("records") var records: List<FitnessCentersRecord>,
+    @SerializedName("_links") var links: Links,
+    @SerializedName("total") var total: Integer
+)
