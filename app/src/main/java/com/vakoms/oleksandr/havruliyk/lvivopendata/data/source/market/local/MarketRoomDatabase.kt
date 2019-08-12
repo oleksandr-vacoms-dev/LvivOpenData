4b65a505de7a74a5a77145595ce3387107cae6d9
oleksandr-vacoms-dev <oleksandr.havruliyk@vakoms.com>
2019-08-12 04:27:53 +0300

    refactoring RoomDatabases and DI AppModule

--- app/src/main/java/com/vakoms/oleksandr/havruliyk/lvivopendata/data/source/market/local/MarketRoomDatabase.kt
+++ app/src/main/java/com/vakoms/oleksandr/havruliyk/lvivopendata/data/source/market/local/MarketRoomDatabase.kt
@@ -1,10 +1,19 @@
 package com.vakoms.oleksandr.havruliyk.lvivopendata.data.source.market.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.vakoms.oleksandr.havruliyk.lvivopendata.data.model.market.MarketRecord
 
 @Database(entities = [MarketRecord::class], version = 1, exportSchema = false)
 abstract class MarketRoomDatabase : RoomDatabase() {
     abstract fun marketDao(): MarketDao
+
+    companion object {
+        fun create(context: Context) = Room
+            .databaseBuilder(context, MarketRoomDatabase::class.java, "market_db")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 }
--- app/src/main/java/com/vakoms/oleksandr/havruliyk/lvivopendata/di/module/AppModule.kt
+++ app/src/main/java/com/vakoms/oleksandr/havruliyk/lvivopendata/di/module/AppModule.kt
@@ -1,7 +1,6 @@
 package com.vakoms.oleksandr.havruliyk.lvivopendata.di.module
 
-import android.content.Context
-import androidx.room.Room
+import android.app.Application
 import com.vakoms.oleksandr.havruliyk.lvivopendata.OpenDataApp
 import com.vakoms.oleksandr.havruliyk.lvivopendata.data.api.OpenDataApi
 import com.vakoms.oleksandr.havruliyk.lvivopendata.data.source.atm.local.ATMRoomDatabase
@@ -9,12 +8,9 @@ import com.vakoms.oleksandr.havruliyk.lvivopendata.data.source.barber.local.Barb
 import com.vakoms.oleksandr.havruliyk.lvivopendata.data.source.catering.local.CateringRoomDatabase
 import com.vakoms.oleksandr.havruliyk.lvivopendata.data.source.fitness.local.FitnessRoomDatabase
 import com.vakoms.oleksandr.havruliyk.lvivopendata.data.source.market.local.MarketRoomDatabase
-import com.vakoms.oleksandr.havruliyk.lvivopendata.util.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import javax.inject.Singleton
@@ -24,64 +20,34 @@ class AppModule {
 
     @Provides
     @Singleton
-    internal fun provideContext(application: OpenDataApp): Context {
-        return application
-    }
+    internal fun provideContext(application: OpenDataApp): Application = application
 
     @Provides
     @Reusable
-    fun provideExecutor(): Executor {
-        return Executors.newSingleThreadExecutor()
-    }
+    fun provideExecutor(): Executor = Executors.newSingleThreadExecutor()
+
 
     @Provides
     @Reusable
-    fun provideOpenDataApi(): OpenDataApi {
-        return OpenDataApi.create()
-    }
+    fun provideOpenDataApi() = OpenDataApi.create()
 
     @Singleton
     @Provides
-    fun provideMarketDb(app: OpenDataApp): MarketRoomDatabase {
-        return Room
-            .databaseBuilder(app, MarketRoomDatabase::class.java, "market_db")
-            .fallbackToDestructiveMigration()
-            .build()
-    }
+    fun provideMarketDb(app: OpenDataApp) = MarketRoomDatabase.create(app)
 
     @Singleton
     @Provides
-    fun provideFitnessDb(app: OpenDataApp): FitnessRoomDatabase {
-        return Room
-            .databaseBuilder(app, FitnessRoomDatabase::class.java, "fitness_db")
-            .fallbackToDestructiveMigration()
-            .build()
-    }
+    fun provideFitnessDb(app: OpenDataApp) = FitnessRoomDatabase.create(app)
 
     @Singleton
     @Provides
-    fun provideCateringDb(app: OpenDataApp): CateringRoomDatabase {
-        return Room
-            .databaseBuilder(app, CateringRoomDatabase::class.java, "catering_db")
-            .fallbackToDestructiveMigration()
-            .build()
-    }
+    fun provideCateringDb(app: OpenDataApp) = CateringRoomDatabase.create(app)
 
     @Singleton
     @Provides
-    fun provideBarberDb(app: OpenDataApp): BarberRoomDatabase {
-        return Room
-            .databaseBuilder(app, BarberRoomDatabase::class.java, "barber_db")
-            .fallbackToDestructiveMigration()
-            .build()
-    }
+    fun provideBarberDb(app: OpenDataApp) = BarberRoomDatabase.create(app)
 
     @Singleton
     @Provides
-    fun provideATMDb(app: OpenDataApp): ATMRoomDatabase {
-        return Room
-            .databaseBuilder(app, ATMRoomDatabase::class.java, "atm_db")
-            .fallbackToDestructiveMigration()
-            .build()
-    }
+    fun provideATMDb(app: OpenDataApp) = ATMRoomDatabase.create(app)
 }
